872ad321de62c4b908c11ef5016fa96d277eaf94
muham <mohamedhesen0@gmail.com>
2023-02-19 10:34:42 +0200

    SecondCommit:Add background work and constraints

--- app/src/main/java/com/udacity/asteroidradar/workmanager/WorkerManager.kt
+++ app/src/main/java/com/udacity/asteroidradar/workmanager/WorkerManager.kt
@@ -1,6 +1,8 @@
 package com.udacity.asteroidradar.workmanager
 
 import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.udacity.asteroidradar.database.AsteroidDataBase
@@ -14,6 +16,7 @@ class WorkerManager(context: Context, parameters: WorkerParameters)
         const val WORK_NAME = "GetAsteroidList"
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override suspend fun doWork(): Result {
 
         val dataBase = AsteroidDataBase.getDataBase(applicationContext)
